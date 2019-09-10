2239781929adf1e59bff5e13657023e7ba3dffc5
saahilshaikh <saahilshaikhrocks@gmail.com>
2019-09-10 12:13:45 +0530

    FILE UPLOAD

---
+++ js/script.js
@@ -0,0 +1,47 @@
+(function ($) {
+    'use strict';
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('header').offset().top > 10) {
+            $('.top-header').addClass('hide');
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.top-header').removeClass('hide');
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    //Hero Slider
+    $('.hero-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 7500,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        infinite: true,
+        arrows: true,
+        fade: true,
+        prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
+        nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>',
+        dots: true
+    });
+    $('.hero-slider').slickAnimation();
+
+    // venobox popup
+    $(document).ready(function () {
+        $('.venobox').venobox();
+    });
+
+    //wow animation
+    new WOW().init();
+
+
+
+})(jQuery);
